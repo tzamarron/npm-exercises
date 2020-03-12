2c1e1a2c62f47a62ac833ba2e046b1f98054bc78
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-12 17:48:54 -0500

    modification

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 
-"use strict";
-const $ = require("jquery");
+const $ = require('jquery');
 const _ = require('lodash');
 
-let hello = hello => console.log("Hello");
+let hello = () => console.log("Hello");
+hello();
 
-$("h2").text("Hello");
+$("h2").text("Yes I can! Auto change that is");
 
-console.log(_.random(1, 10));
+let testArray = [1,2,3,4,5,6,7,8,9,10];
+//let odds = _.remove(testArray,(n)=> n%2 === 0);
+//let reduceArray = _.reduce(testArray,(sum,n)=> sum + n,0);
+//_.forEach(testArray,(value) => console.log(value));
+// let random = _.random(1,5,false);
+
+// console.log(testArray);
+//console.log(odds);
+//console.log(reduceArray);
+// console.log(random);
